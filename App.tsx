6a7ce4dd338868ce6f87a15f9a5ee300e9043b6f
kopley8-jpg <Kopley8@gmail.com>
2026-02-06 15:28:53 +0300

    AppChanges

--- App.tsx
+++ App.tsx
@@ -1,45 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useState } from "react"
+import { ColorValue, Text, View } from "react-native"
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const App = () => {
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [col, setCol] = useState<boolean>(false)
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+  return(
+    <View style={{flex:1, backgroundColor:col?"white":"yellow", alignItems:"center", justifyContent:"center"}} onTouchStart={() => setCol(cCol => !cCol)}>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default App
