12b5fef928517a4191bad007e709435e0c034220
kopley8-jpg <Kopley8@gmail.com>
2026-02-14 02:40:07 +0300

    dom

--- src/shared/ColorPath.tsx
+++ src/shared/ColorPath.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { LayoutRectangle, StyleSheet, View } from "react-native"
+import { GestureResponderEvent, LayoutChangeEvent, LayoutRectangle, StyleSheet, useWindowDimensions, View } from "react-native"
 
 import { AnimatedColor } from "../features/led-control/ui/EffectEditor/interfaces"
 import LinearGradient from "react-native-linear-gradient"
@@ -12,25 +12,32 @@ interface IColorPath {
 
 export const ColorPath:React.FC<IColorPath> = ({animatedColor, onThumbTouchIn, onThumbsContainerTouchMove}) => {
 
-    const [widthThumbsContainer, setWidthThumbsContainer] = useState<number>(100)
+    const [thumbsContainerWidth, setThumbsContainerWidth] = useState(100)
+    const windowWidth = useWindowDimensions().width
 
-    
+    const handleLayout = (e:LayoutChangeEvent) => {
+        setThumbsContainerWidth(e.nativeEvent.layout.width)
+    }
 
-    const handleThumbsContainerTouchMove = (x:number) => {
-        console.log(x/(widthThumbsContainer/100)/100)
+    const handleThumbsContainerTouchMove = (e:GestureResponderEvent) => {
+        const offset = ((e.nativeEvent.pageX-((windowWidth-thumbsContainerWidth)/2))/(thumbsContainerWidth/100)/100)
+        console.log(e.nativeEvent.pageX)
+        if(!(e.nativeEvent.pageX<((windowWidth-thumbsContainerWidth)/2)||e.nativeEvent.pageX>(windowWidth-((windowWidth-thumbsContainerWidth)/2)))){
+            onThumbsContainerTouchMove(offset)
+        }
     }
 
     return(
         <View style={styles.container}>
-            <View style={styles.thumbsContainer}
-            onTouchStart={p => handleThumbsContainerTouchMove(p.nativeEvent.locationX)} 
-            onLayout={p => setWidthThumbsContainer(p.nativeEvent.layout.width)}>
+            <View 
+            style={styles.thumbsContainer}
+            onLayout={handleLayout}
+            onTouchMove={handleThumbsContainerTouchMove}>
                 {animatedColor.stops.map(stop => (
                     <View 
-                    key={stop.id} 
-                    style={{width:"4%", height:"80%", backgroundColor:stop.color, position:"absolute", left:`${stop.offset*100-2}%`}} 
-                    onTouchStart={() => onThumbTouchIn(stop.id)}
-                    />
+                    key={stop.id}
+                    onTouchStart={() => onThumbTouchIn(stop.id)} 
+                    style={{width:"6%", height:"90%", backgroundColor:stop.color, position:'absolute', left:`${stop.offset*100-3}%`}}/>
                 ))}
             </View>
             <LinearGradient 
@@ -39,11 +46,11 @@ export const ColorPath:React.FC<IColorPath> = ({animatedColor, onThumbTouchIn, o
             colors={animatedColor.stops.map(res => res.color)} 
             locations={animatedColor.stops.map(res => res.offset)} 
             style={styles.colorPathContainer}>
-
             </LinearGradient>
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     container:{
